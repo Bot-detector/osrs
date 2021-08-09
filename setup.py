3bb7b0d8767a817dbb4fc07c4a39d425d5dcca61
extreme4all <40169115+extreme4all@users.noreply.github.com>
2021-08-09 19:03:15 +0200

    0.0.1 => 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="osrs",
-    version="0.0.1",
+    version="0.0.2",
     author="extreme4all",
     author_email="",
     description="Simple Wrapper for osrs related api's",
