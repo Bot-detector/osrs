2063c5b1f7f1937118646f6d93d1d0257288984f
extreme4all <40169115+extreme4all@users.noreply.github.com>
2021-08-09 20:13:57 +0200

    osrs

---
+++ osrs/Osrs.py
@@ -0,0 +1,44 @@
+import requests
+import time
+import logging
+
+class osrsPrices:
+    def __init__(self, header):
+        self.header = header
+        self.base_url = 'https://secure.runescape.com/m=itemdb_oldschool/api/'
+        self.last_call = None
+
+    def timeseries(self, item_id):
+        '''
+            get timeseries of prices for an item
+        '''
+        now = time.time()
+        if self.last_call and now - self.last_call < 2:
+            logging.warning('rate limiter may apply')
+
+        url = f'/graph/{self.base_url}{item_id}.json'
+        data = requests.get(url, headers=self.header)
+        self.last_call = now
+        
+        if len(data.text) == 0:
+            raise ValueError('rate limiter applied by jagex')
+
+        data = data.json()
+
+        # converting data in a more verbose way
+        daily = [{'timestamp': key, 'price': value } for key, value in data['daily'].items()]
+        average = [{'timestamp': key, 'price': value } for key, value in data['average'].items()]
+
+        # recreate dictionary
+        data = {}
+        data['daily'] = daily
+        data['average'] = average
+        return data
+
+
+if __name__ == "__main__":
+    header = {'user-agent':'extreme4all#6456'}
+
+    api = osrsPrices(header=header)
+    d = api.items(first_letter="a")
+    print(d)
