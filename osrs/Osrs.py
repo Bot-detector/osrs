af64e2b80bc256d862fca7f726e2308cf1fe0363
extreme4all <40169115+extreme4all@users.noreply.github.com>
2021-08-09 20:40:18 +0200

    osrs api

--- osrs/Osrs.py
+++ osrs/Osrs.py
@@ -2,11 +2,41 @@ import requests
 import time
 import logging
 
+
 class osrsPrices:
     def __init__(self, header):
         self.header = header
-        self.base_url = 'https://secure.runescape.com/m=itemdb_oldschool/api/'
+        self.base_url = 'https://secure.runescape.com/m=itemdb_oldschool/api'
         self.last_call = None
+    
+    def category(self):
+        url = f'{self.base_url}/catalogue/category.json?category=1'
+        data = requests.get(url, headers=self.header)
+        
+        if len(data.text) == 0:
+            raise ValueError('rate limiter applied by jagex')
+        return data.json()
+
+    def items(self, letter, page=0):
+        '''
+            for each letter returns 12 items per page
+        '''
+        url = f'{self.base_url}/catalogue/items.json?category=1&alpha={letter}&page={page}'
+        data = requests.get(url, headers=self.header)
+
+        if len(data.text) == 0:
+            raise ValueError('rate limiter applied by jagex')
+        
+        return data.json()
+
+    def itemDetail(self, item_id):
+        url = f'{self.base_url}/catalogue/detail.json?item={item_id}'
+        data = requests.get(url, headers=self.header)
+
+        if len(data.text) == 0:
+            raise ValueError('rate limiter applied by jagex')
+        
+        return data.json()
 
     def timeseries(self, item_id):
         '''
@@ -16,7 +46,7 @@ class osrsPrices:
         if self.last_call and now - self.last_call < 2:
             logging.warning('rate limiter may apply')
 
-        url = f'/graph/{self.base_url}{item_id}.json'
+        url = f'{self.base_url}/graph/{item_id}.json'
         data = requests.get(url, headers=self.header)
         self.last_call = now
         
@@ -40,5 +70,7 @@ if __name__ == "__main__":
     header = {'user-agent':'extreme4all#6456'}
 
     api = osrsPrices(header=header)
-    d = api.timeseries(item_id=2)
-    print(d)
+    # print(api.timeseries(2))
+    # print(api.category())
+    # print(api.items('a'))
+    print(api.itemDetail(4151))
