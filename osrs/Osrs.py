526ce1b39056cdc25fce66b9907616d2820b9ff7
extreme4all <40169115+extreme4all@users.noreply.github.com>
2021-08-10 13:41:23 +0200

    Update Osrs.py

--- osrs/Osrs.py
+++ osrs/Osrs.py
@@ -1,59 +1,66 @@
-import requests
-import time
 import logging
+import time
 
+import requests
 
 class osrsPrices:
-    def __init__(self, header):
+    def __init__(self, header, respect_rate_limiter=True):
         self.header = header
         self.base_url = 'https://secure.runescape.com/m=itemdb_oldschool/api'
         self.last_call = None
+        self.respect_rate_limiter = respect_rate_limiter
     
-    def category(self):
-        url = f'{self.base_url}/catalogue/category.json?category=1'
-        data = requests.get(url, headers=self.header)
-        
-        if len(data.text) == 0:
-            raise ValueError('rate limiter applied by jagex')
-        return data.json()
-
-    def items(self, letter, page=0):
-        '''
-            for each letter returns 12 items per page
-        '''
-        url = f'{self.base_url}/catalogue/items.json?category=1&alpha={letter}&page={page}'
-        data = requests.get(url, headers=self.header)
-
-        if len(data.text) == 0:
-            raise ValueError('rate limiter applied by jagex')
-        
-        return data.json()
-
-    def itemDetail(self, item_id):
-        url = f'{self.base_url}/catalogue/detail.json?item={item_id}'
-        data = requests.get(url, headers=self.header)
-
-        if len(data.text) == 0:
-            raise ValueError('rate limiter applied by jagex')
-        
-        return data.json()
-
-    def timeseries(self, item_id):
-        '''
-            get timeseries of prices for an item
-        '''
+    def __webcall(self, url):
         now = time.time()
-        if self.last_call and now - self.last_call < 2:
-            logging.warning('rate limiter may apply')
+        second_per_call = 2
 
-        url = f'{self.base_url}/graph/{item_id}.json'
+        if self.last_call:
+            wait = now - self.last_call
+            if wait < second_per_call:
+                if self.respect_rate_limiter:
+                    sleep = second_per_call - wait
+                    logging.debug(f'Respecting rate limiter sleeping: {sleep}')
+                    time.sleep(sleep)
+                else:
+                    logging.warning('rate limiter may apply')
+
         data = requests.get(url, headers=self.header)
         self.last_call = now
         
         if len(data.text) == 0:
             raise ValueError('rate limiter applied by jagex')
 
-        data = data.json()
+        return data
+
+    def images(self, item_id):
+        '''
+            get the icon's for an item
+        '''
+        icon = self.__webcall(f'https://secure.runescape.com/m=itemdb_oldschool/obj_sprite.gif?id={item_id}')
+        icon_large = self.__webcall(f'https://secure.runescape.com/m=itemdb_oldschool/obj_big.gif?id={item_id}')
+        return icon, icon_large
+        
+    def category(self):
+        url = f'{self.base_url}/catalogue/category.json?category=1'
+        return self.__webcall(url).json()
+
+    def items(self, letter, page=0):
+        '''
+            for each letter returns 12 items per page
+        '''
+        url = f'{self.base_url}/catalogue/items.json?category=1&alpha={letter}&page={page}'
+        return self.__webcall(url).json()
+
+    def itemDetail(self, item_id):
+        url = f'{self.base_url}/catalogue/detail.json?item={item_id}'
+        return self.__webcall(url).json()
+
+    def timeseries(self, item_id):
+        '''
+            get timeseries of prices for an item
+        '''
+        url = f'{self.base_url}/graph/{item_id}.json'
+        data = self.__webcall(url).json()
 
         # converting data in a more verbose way
         daily = [{'timestamp': key, 'price': value } for key, value in data['daily'].items()]
@@ -73,4 +80,5 @@ if __name__ == "__main__":
     # print(api.timeseries(2))
     # print(api.category())
     # print(api.items('a'))
-    print(api.itemDetail(4151))
+    # print(api.itemDetail(4151))
+    print(api.images(4151))
