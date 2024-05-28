809442f8546750e7b9acc37ebf8e6a7cc3b0a8c0
extreme4all <>
2024-05-28 21:38:29 +0200

    added fail test

--- tests/test_hiscore.py
+++ tests/test_hiscore.py
@@ -1,5 +1,6 @@
 import pytest
 from aiohttp import ClientSession
+from aiohttp.client_exceptions import ClientResponseError
 
 from osrs.hiscore import HiScore
 
@@ -14,8 +15,16 @@ async def test_get_ranking_success(aiohttp_session: ClientSession):
 
 
 @pytest.mark.asyncio
-async def test_get_hiscore(aiohttp_session: ClientSession):
+async def test_get_hiscore_success(aiohttp_session: ClientSession):
     async for session in aiohttp_session:
         hiscore_instance = HiScore(session=session)
         data = await hiscore_instance.get_hiscore(player="extreme4all")
         assert isinstance(data, dict)
+
+
+@pytest.mark.asyncio
+async def test_get_hiscore_fail(aiohttp_session: ClientSession):
+    async for session in aiohttp_session:
+        hiscore_instance = HiScore(session=session)
+        with pytest.raises(ClientResponseError):
+            await hiscore_instance.get_hiscore(player="aaa-bbb-ccc-ddd-eee-fff")
